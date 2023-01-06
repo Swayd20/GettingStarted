2f7bcb04743bd6b95dcf44e94b0d2316099851bf
Ivan Dong <swayd@DESKTOP-9ED3CBP>
2023-01-06 15:31:36 -0500

    Just added some random strings

---
+++ NewStuff/RandomStrings.py
@@ -0,0 +1,7 @@
+print("Hello \"你好\" ")
+greetings_eng = "hello"
+greetings_cn = "你好"
+
+print(greetings_eng.upper() + " is " + greetings_cn + " in english")
+print(greetings_eng.upper()[4])
+print(greetings_eng.index("h"))
